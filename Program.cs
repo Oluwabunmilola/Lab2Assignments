4e074fafb340d485e93039c630bedd73dd8a17ad
Precious <oluwabunmipreciousajayi@gmail.com>
2021-04-26 18:07:09 +0100

    lab 2 assignment

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LAB_2_ASSIGNMENTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //    Console.WriteLine("Hello World!");
+
+        
+//         int a=20;
+//         int b=3;
+//         int c=a%b;
+
+// Console.WriteLine(c);
+            
+            Console.WriteLine("Input first no: ");
+            var inputNo1=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input second no: ");
+            var inputNo2=Convert.ToInt32(Console.ReadLine());
+
+            var returnReminder=inputNo1 % inputNo2;
+            Console.WriteLine("The reminder is: " + returnReminder);
+
+
+        }
+    }
+}
